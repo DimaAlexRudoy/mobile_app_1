c1c8a9bbc1ce541d779d7e6704d799665858377c
DimaAlexRudoy <83850991+DimaAlexRudoy@users.noreply.github.com>
2023-05-14 19:44:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Main(App):
+    def build(self):
+        return Label(text='Hello world', size_hint=(.5, .5), pos_hint={'center_x':.5, 'center_y':.5})
+    
+if __name__ == '__main__':
+    app = Main()
+    app.run()
